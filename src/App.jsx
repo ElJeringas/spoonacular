5f55f9af6a46e9594206b0fcea65e673baf6d324
ElJeringas <santiagojss32@gmail.com>
2023-07-10 22:22:22 -0500

    Adding routes and new category pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
 import Pages from "./pages/Pages"
-
+import "../index.css"
+import Category from "./components/Category"
 function App() {
 
   return (
     <div className="index">
-    <h1>Hello</h1>
+    <h1>All Recipes</h1>
+    <Category/>
     <Pages />
     </div>
   )
